1b045355629b89558651ed1f5444183aea1c1745
brokerr1 <solovievdima1@gmail.com>
2022-06-28 20:35:40 +0300

    метод по подсчету нужных нам элементов заданного массива

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -2,6 +2,20 @@
 string[] arrString = CreateArrayString();
 Console.WriteLine("Дан исходный массив:");
 PrintArrayString(arrString);
+Console.WriteLine("Идет обработка массива ...");
+string[] newArrString = CorrectionArrayString(arrString);
+string[] CorrectionArrayString(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] newArray = new string[count];
+}
 void PrintArrayString(string[] array)
 {
     //Console.WriteLine("Сформированный массив:");
