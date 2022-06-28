b27d667e415d015defdb5bef44e67ef9433af77d
brokerr1 <solovievdima1@gmail.com>
2022-06-28 20:32:56 +0300

    реализация метода ввода массива с клавиатуры

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿Console.WriteLine("Создание исходного массива ...");
+string[] arrString = CreateArrayString();
+string[] CreateArrayString()
+{ 
+    Console.Write("Введите слова через пробел:");
+    string[] array = Console.ReadLine().Split().ToArray();
+    return array;
+}
