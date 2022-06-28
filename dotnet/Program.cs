ec4d27b83fa2b697a7fbef65cb282703733b3f0d
brokerr1 <solovievdima1@gmail.com>
2022-06-28 20:34:02 +0300

    реализация метода печати массива

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,5 +1,16 @@
 ﻿Console.WriteLine("Создание исходного массива ...");
 string[] arrString = CreateArrayString();
+Console.WriteLine("Дан исходный массив:");
+PrintArrayString(arrString);
+void PrintArrayString(string[] array)
+{
+    //Console.WriteLine("Сформированный массив:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("{0} ", array[i]);
+    }
+    Console.WriteLine();
+}
 string[] CreateArrayString()
 { 
     Console.Write("Введите слова через пробел:");
